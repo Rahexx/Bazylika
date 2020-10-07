877cb663c2ffddaa08c39daecea7baeb5280b0d3
Andrzej Łuszcz <endriu1297@gmail.com>
2020-10-07 21:04:56 +0200

    create style for horizontal mobile view

--- src/index.js
+++ src/index.js
@@ -2,20 +2,56 @@ import './sass/main.scss';
 import { gsap } from 'gsap';
 
 const burger = document.querySelector('.burger');
+const verticalMenu = document.querySelector('.mobileNav__item--vertical');
 const nav = document.querySelector('.mainNavigation');
+let isVertical = false;
+
+function changeBurgerClasses(item, isOpen) {
+  if (isOpen) {
+    item.classList.remove('fa-bars');
+    item.classList.add('fa-times');
+  } else {
+    item.classList.add('fa-bars');
+    item.classList.remove('fa-times');
+  }
+}
+
+function showMenu(item, width) {
+  console.log(item);
+  item.classList.add('active');
+  gsap.to('.mainNavigation', { x: width, duration: 1 });
+  changeBurgerClasses(item.children[0], true);
+}
+
+function closeMenu(item) {
+  item.classList.remove('active');
+  gsap.to('.mainNavigation', { x: 0, duration: 1 });
+  item.children[0].classList.add('fa-bars');
+  item.children[0].classList.remove('fa-times');
+  changeBurgerClasses(item.children[0], false);
+}
+
+verticalMenu.addEventListener('click', () => {
+  const pageWidth = document.body.offsetWidth;
+  const icon = document.querySelector('.mobileNav__icon--menu');
+
+  if (!burger.classList.contains('active')) {
+    showMenu(burger, pageWidth);
+    changeBurgerClasses(icon, true);
+  } else {
+    closeMenu(burger);
+    icon.classList.add('fa-bars');
+    icon.classList.remove('fa-times');
+    changeBurgerClasses(icon, false);
+  }
+});
 
 burger.addEventListener('click', () => {
   const pageWidth = document.body.offsetWidth;
 
   if (!burger.classList.contains('active')) {
-    burger.classList.add('active');
-    gsap.to('.mainNavigation', { x: pageWidth, duration: 1 });
-    burger.children[0].classList.remove('fa-bars');
-    burger.children[0].classList.add('fa-times');
+    showMenu(burger, pageWidth);
   } else {
-    burger.classList.remove('active');
-    gsap.to('.mainNavigation', { x: 0, duration: 1 });
-    burger.children[0].classList.add('fa-bars');
-    burger.children[0].classList.remove('fa-times');
+    closeMenu(burger);
   }
 });
