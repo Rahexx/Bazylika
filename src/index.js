21e0b15a2ecebeac0cf510f1b4662f956e5abc5f
Andrzej Łuszcz <endriu1297@gmail.com>
2020-10-07 18:42:48 +0200

    added animation show and close navigation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './sass/main.scss';
+import { gsap } from 'gsap';
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.mainNavigation');
+
+burger.addEventListener('click', () => {
+  const pageWidth = document.body.offsetWidth;
+
+  if (!burger.classList.contains('active')) {
+    burger.classList.add('active');
+    gsap.to('.mainNavigation', { x: pageWidth, duration: 1 });
+    burger.children[0].classList.remove('fa-bars');
+    burger.children[0].classList.add('fa-times');
+  } else {
+    burger.classList.remove('active');
+    gsap.to('.mainNavigation', { x: 0, duration: 1 });
+    burger.children[0].classList.add('fa-bars');
+    burger.children[0].classList.remove('fa-times');
+  }
+});
